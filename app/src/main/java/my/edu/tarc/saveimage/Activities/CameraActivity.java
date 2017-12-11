a4446ef37a559e35ff46139bd5b4781a8ce8128e
SeeKT <seekt@acd.tarc.edu.my>
2017-12-11 16:09:09 +0800

    Use HTTP

---
+++ app/src/main/java/my/edu/tarc/saveimage/Activities/CameraActivity.java
@@ -0,0 +1,178 @@
+package my.edu.tarc.saveimage.Activities;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import my.edu.tarc.saveimage.WebServices.HTTPDataHandler;
+import my.edu.tarc.saveimage.R;
+import my.edu.tarc.saveimage.WebServices.HTTPURLConnection;
+
+import static android.R.attr.bitmap;
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
+public class CameraActivity extends AppCompatActivity {
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 3;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final String IMAGE_DIRECTORY_NAME = "SavaImage";
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL = 2;
+    private Uri fileUri; // file url to store image/video
+    private ImageView imagePreview;
+    Bitmap bitmap;
+    String imageString;
+
+    private HTTPURLConnection service;
+    private JSONObject json;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        imagePreview = (ImageView) findViewById(R.id.imageViewPreview);
+
+        // Assume thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            imagePreview.setImageBitmap(bitmap);
+
+            //converting image to base64 string
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] imageBytes = baos.toByteArray();
+            imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+        }
+    }
+
+    public void uploadImage(View view){
+        new PostDataTOServer().execute();
+    }
+
+    private class PostDataTOServer extends AsyncTask<Void, Void, Void> {
+        String response = "";
+
+        //Create hashmap Object to send parameters to web service
+        HashMap<String, String> postDataParams;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            try {
+                postDataParams = new HashMap<String, String>();
+                postDataParams.put("image", imageString);
+                service = new HTTPURLConnection();
+                response = service.ServerData(getString(R.string.url_insert_image), postDataParams);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ app/src/main/java/my/edu/tarc/saveimage/Utilities/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package my.edu.tarc.saveimage.Utilities;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by TAR UC on 11/2/2017.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
